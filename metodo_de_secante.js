5b2fcd4eacc8d363e98096424e7bed5a7d1aeab0
Brandonalexiscc <90300293+Brandonalexiscc@users.noreply.github.com>
2022-05-12 17:33:25 -0500

    correccion1

--- metodo_de_secante.js
+++ metodo_de_secante.js
@@ -7,27 +7,37 @@ const btnRes = document.getElementById('btnRes')
 
 function iteraciones(){
     let error =  100;
+    let n = 0;
+    let cf = input('ingresar funcion')
+    let f = inline(cf);
+    let x0 = input('ingresar primer valor: ')
     let tolerancia =  parseFloat(document.getElementById('valor6').value);
     let Xi = parseFloat(document.getElementById('valor1').value);
     let Xi1= parseFloat(document.getElementById('valor2').value);
     let Fxi= parseFloat(document.getElementById('valor3').value);
     let Fx1= parseFloat(document.getElementById('valor4').value);
-    
+    printf('    n   x0  x1  x2  error\n');
+    printf('    %1   %f  %f  x2  %f\n');
+
     while(error <= tolerancia ){
         let xr = Xi1 - Xi;
         let xr2 = Fxi * xr;
         let xr3 = Fx1 - Fxi;
         let xr4 = xr2 / xr3;
         let Xr = Xi - xr4;
+        n = n++;
 
         let x2 = Xr;
-        let x0 = x1;
-        let x1 = x2;
+         x0 = x1;
+         x1 = x2;
 
-        error = Math.abs( )
+        error = Math.abs(x2);
 
     }
 
     document.getElementById('el-resultado').innerHTML = toString();
     console.log(res);
+}
+function orden(X0, X1){
+
 }
