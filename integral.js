5ca971f0690fca0319c90b6592a763b0f3ca5e43
Brandonalexiscc <90300293+Brandonalexiscc@users.noreply.github.com>
2022-05-26 00:50:29 -0500

    9

--- integral.js
+++ integral.js
@@ -8,24 +8,16 @@ function integralDefinida(){
     let Xi = parseFloat(document.getElementById('valor1').value); //a
     let Xi1= parseFloat(document.getElementById('valor2').value); //b
     
-    let Fxi = fx(Xi);
-    let Fx1 = fx(Xi1);
-
-    Fxi = fx(Xi);
-    Fx1 = fx(Xi1);
-
-    let procedimiento1 = (-((Fxi ** 3) / 3) - ((Fxi ** 2 ) / 2));
-    let procedimiento2 = (-((Fx1 ** 3) / 2 ) - ((Fx1 ** 2) / 2 ));
+    let procedimiento1 = ((Xi ** 3) / 3) - ((Xi ** 2 ) / 2);
+    let procedimiento2 = ((Xi1 ** 3) / 3 ) - ((Xi1 ** 2) / 2 );
 
     let resta = procedimiento2 - procedimiento1;
 
-    
+    let total = resta;
 
-    document.getElementById('el-resultado').innerHTML = resta.toString() ;
+
+    document.getElementById('el-resultado').innerHTML = total.toString() ;
 }
 
-    function fx(x){
-    return (Math.pow(x, 2) - x);
-}
 
 console.log(res);
